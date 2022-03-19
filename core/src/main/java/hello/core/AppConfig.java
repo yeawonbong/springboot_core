b68a6a977f4dc4662fb90fcd9016e28a41b16805
yeawonbong <ybong.develop@gmail.com>
2022-03-19 23:31:39 +0900

    0319_refactoring_AppConfig

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -2,6 +2,7 @@ package hello.core;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
@@ -26,7 +27,8 @@ public class AppConfig {
     }
 
     public DiscountPolicy discountPolicy() {
-        return new FixDiscountPolicy();
+//        return new FixDiscountPolicy(); // 이 부분만 고치면 갈아낄 수 있다.
+        return new RateDiscountPolicy();
     }
 }
 
